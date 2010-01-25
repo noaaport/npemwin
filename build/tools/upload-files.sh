c363e6ce1707010e9a294611125cbc1992998e9b
nieves <>
2010-01-25 20:43:55 +0000

    Added support for nbspupdate.

--- build/tools/upload-files.sh
+++ build/tools/upload-files.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
-
+#
+# $Id$
+#
 . upload.conf
 
 cp ../../RELEASE_NOTES ../../conf/*.README .
 upload_files="RELEASE_NOTES *.README"
 
-readme_file=RELEASE_NOTES
+release_file=RELEASE_NOTES
 dt=`date +%d%b%G`
 
-ftp -n -v $uploadhost <<EOF
-user $uploaduser
-prompt
-mkdir $uploaddir
-cd $uploaddir
-mkdir Docs
-cd Docs
-mdelete *
+lftp -c "\
+$lftpoptions;
+open -u $uploaduser $uploadhost;
+cd $uploadbasedir;
+mkdir -p $filesdir;
+cd $filesdir;
+mrm *
 mput $upload_files
-rename $readme_file $readme_file-$dt
-quit
-EOF
+mv $release_file $release_file-$dt
+quit"
 
 rm $upload_files
