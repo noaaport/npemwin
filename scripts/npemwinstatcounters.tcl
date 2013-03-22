4aef1abde4066f812b59fe7ba83d8ed094b12066
nieves <nieves@volkron.net>
2013-03-22 10:33:51 -0400

    Revised npemwinstatcounters

---
+++ scripts/npemwinstatcounters.tcl
@@ -0,0 +1,218 @@
+#!%TCLSH%
+#
+# $Id$
+#
+# Usage: npemwinstatcounters [-f <fmt>] [-a <activefile>]
+#                            [-e <emwinstatusfile>] [-s <statusfile>]
+#
+# npemwin counters in the last minute (ending at "unix_seconds").
+#
+# The default files are:
+#
+#   <activefile>  = npemwind.active
+#   <emwinstatusfile> = servers.status
+#   <statusfile>  = npemwind.status
+#
+# The last line is written to stdout; the order of the columns is given below.
+# The <fmt> can be: std (default), stdh, xml, csv, csvk
+# The motivation for the existence of this tool is to use it for extracting
+# and feeding the data to rrdtool or similar programs
+# (it is used by the _iemwin extension in the web interface).
+
+package require cmdline;
+set usage {npemwintatcounters [-f <fmt>] [-a <activefile>]
+    [-e <emwinstatusfile>] [-s <statusfile>]};
+
+set optlist {{a.arg ""} {e.arg ""} {f.arg "std"} {s.arg ""}};
+
+proc err {s} {
+
+    global argv0;
+
+    puts "Error: $s";
+    exit 1;
+}
+
+#
+# auxiliary functions
+#
+proc _output_stats_connections {file} {
+#
+# This function returns a tcl list, in which each element is the data
+# for each connected client. The code is copied from npemwin_connections{}
+# in functions.tcl in the tclhttpd main lib.
+# The data for each client is: "ip" "type" "queue" "time"
+#
+    set r [list];
+    set f [open $file r];
+    while {[gets $f finfo] > 0} {
+
+        if {[regexp {^(-|\s*$)} $finfo]} {
+            continue;
+        }
+
+        array set a [proc_stringtoarray [string trimleft $finfo]];
+        set client_data [format "%s %s %s %s" $a(1) $a(2) $a(3) $a(4)];
+	lappend r $client_data;
+
+    }
+    close $f;
+
+    return $r;
+}
+
+proc _get_upstream_stats {file} {
+
+    set upstream_servers_status_list [split [exec cat $file] "\n"];
+    
+    # The current server is the one with the disconnect time equal to 0
+    foreach server $upstream_servers_status_list {
+	set disconnect_time [lindex $server 3];
+	if {$disconnect_time == 0} {
+	    break;
+	}
+    }
+
+    return $server;
+}
+
+## The common defaults
+set _defaultsfile "/usr/local/etc/npemwin/filters.conf";
+if {[file exists ${_defaultsfile}] == 0} {
+    err "${_defaultsfile} not found.";
+}
+source ${_defaultsfile};
+unset _defaultsfile;
+
+# npemwind.init is needed for the stats files and variables (log periods)
+set npemwind_init_file [file join $gf(filterdir) npemwind.init];
+if {[file exists $npemwind_init_file] == 0} {
+    err "$npemwind_init_file not found.";
+}
+source $npemwind_init_file;
+unset npemwind_init_file;
+
+array set option [::cmdline::getoptions argv $optlist $usage];
+set argc [llength $argv];
+
+# defaults
+set fmt $option(f);
+set statsperiod_secs $npemwind(statsperiod);    # seconds
+set pidfile $npemwind(pidfile);
+set activefile $npemwind(serveractivefile);
+set statusfile $npemwind(statusfile);
+set emwinstatusfile $npemwind(emwinstatusfile);
+#
+# not configurable
+#
+set data_format 2;
+
+# Initialization
+set npemwind_start_time "";                     # calculated from pid file
+set status_values [list];
+set estatus_values [list];
+set num_clients "";                             # "" means undetermined
+set client_table "";
+ 
+if {$option(a) ne ""} {
+    set activefile $option(a);
+}
+
+if {$option(e) ne ""} {
+    set emwinstatusfile $option(e);
+}
+
+if {$option(s) ne ""} {
+    set statusfile $option(s);
+}
+
+# The data is prepended by meta data (data_format, server start time,
+#                                     log period)
+if {[file exists $pidfile]} {
+    set npemwind_start_time [file mtime $pidfile];
+}
+
+set keywords [list data_format npemwind_start_time statsperiod_secs];
+set values [list $data_format $npemwind_start_time $statsperiod_secs];
+
+#
+# These are the fields of the npemwind.status file, followed by the 
+# servers.status file.
+#
+lappend keywords \
+    stats_time \
+    frames_received \
+    frames_processed \
+    frames_bad \
+    frames_data_size \
+    es_ip \
+    es_port \
+    es_stats_connect \
+    es_stats_disconnect \
+    es_stats_consecutive_packets \
+    es_stats_max_packets \
+    es_stats_total_packets \
+    es_stats_bad_packet_count \
+    es_stats_connections \
+    es_stats_error \
+    es_stats_serverread_errors \
+    es_stats_serverclosed_errors \
+    es_stats_header_errors \
+    es_stats_checksum_errors \
+    es_stats_filename_errors \
+    es_stats_unknown_errors;
+
+# Include the upstream conection info and the client table
+lappend keywords num_clients client_table;
+
+if {[file exists $statusfile]} {
+    set status_values [split [exec tail -n 1 $statusfile]];
+}
+
+if {[file exists $emwinstatusfile]} {
+    set estatus_values [split [_get_upstream_stats $emwinstatusfile]];
+}
+
+# The client table
+if {[file exists $activefile]} {
+    set _client_data_list [_output_stats_connections $activefile];
+    set num_clients [llength ${_client_data_list}];
+    set client_table [join ${_client_data_list} "|"];
+}
+
+set values [concat $values \
+		$status_values \
+		$estatus_values \
+		$num_clients \
+		$client_table];
+
+if {$fmt eq "stdh"} {
+    puts {#
+# npemwin counters in the last period (ending at "stats_time" in unix seconds).
+#
+DATA_START};
+
+    foreach k $keywords v $values {
+	puts "$k=$v";
+    }
+    puts "DATA_END";
+} elseif {$fmt eq "std"} {
+    foreach k $keywords v $values {
+	puts "$k=$v";
+    }
+} elseif {$fmt eq "csv"} {
+    puts [join $values ","];
+} elseif {$fmt eq "csvk"} {
+    set r "";
+    foreach k $keywords v $values {
+	append r "$k=$v,";
+    }
+    puts [string trim $r ","];
+} elseif {$fmt eq "xml"} {
+    foreach k $keywords v $values {
+	puts "<$k>$v</$k>";
+    }
+} else {
+    puts "Invalid format: $fmt";
+    return 1;
+}
--- tclhttpd/extensions/iemwin/iemwin.tcl
+++ tclhttpd/extensions/iemwin/iemwin.tcl
@@ -7,7 +7,7 @@
 #
 # where <command> is
 #
-# /_iemwin/stats[?format=xml|csv]
+# /_iemwin/stats[?format=stdh|std|xml|csv|csvk]
 #
 Direct_Url /_iemwin _iemwin;
 
@@ -15,18 +15,9 @@ set iemwin(conf) "iemwin.conf";
 set iemwin(confdir) $Config(confdir);
 set iemwin(localconfdirs) $Config(localconfdirs);
 #
-set iemwin(upstream_master_file) $Config(npemwinemwinstatusfile);
-set iemwin(npemwin_status_file) $Config(npemwinstatusfile);
-set iemwin(active_clients_file) $Config(npemwinserveractivefile);
-set iemwin(npemwind_pidfile) $Config(npemwinpidfile);
-
 # Non-configurable
 #
-# 1 => original (up to npemwin-2.4.5r)
-# 2 => includes the frames stats (npemwin_status keyword) (npemwin-2.4.6r)
-#
 set iemwin(data_type) "npemwin/_iemwin/stats";  # <app><url>
-set iemwin(data_format) 2;                      # version number
 
 # The local overrides
 set _iemwinconf [file join $iemwin(confdir) $iemwin(conf)];
@@ -35,7 +26,7 @@ if {[file exists ${_iemwinconf}]} {
 }
 unset _iemwinconf;
 
-proc _iemwin/stats {{format "csv"}} {
+proc _iemwin/stats {{format "std"}} {
 
     global iemwin;
 
@@ -45,8 +36,7 @@ proc _iemwin/stats {{format "csv"}} {
 
     if {$format eq "xml"} {
 	set _iemwin/stats "text/xml";
-	set type "stats";
-	set r [iemwin_output_xml_start $type];
+	set r [iemwin_output_xml_start $iemwin(data_type)];
     } else {
 	set r "";
     }
@@ -54,7 +44,7 @@ proc _iemwin/stats {{format "csv"}} {
     append r [iemwin_output_stats $format];
 
     if {$format eq "xml"} {
-	append r [iemwin_output_xml_end $type];
+	append r [iemwin_output_xml_end $iemwin(data_type)];
     }
 
     return $r;
@@ -64,104 +54,45 @@ proc iemwin_output_stats {format} {
 
     global iemwin;
 
-    foreach f [list upstream_master_file \
-		   npemwin_status_file \
-		   active_clients_file \
-		   npemwind_pidfile] {
+    set status [catch {
+	set data [exec npemwinstatcounters -f $format];
+    } errmsg];
 
-	if {[file exists $iemwin($f)] == 0} {
-	    return "";
-	}
+    if {$status != 0} {
+	return "";
     }
 
-    set data_type $iemwin(data_type);
-    set data_format $iemwin(data_format);
-    set start_time [file mtime $iemwin(npemwind_pidfile)];
-    set upstream_master [iemwin_get_upstream_master];
-    set npemwin_status [exec tail -n 1 $iemwin(npemwin_status_file)];
-
-    set _client_data_list \
-	[iemwin_output_stats_connections $iemwin(active_clients_file)];
-    set num_clients [llength ${_client_data_list}];
-    set client_table [join ${_client_data_list} "\n"];
-
+    # Prepend the data_type and any other metadata before the data
+    # output by npemwinstatcounters (unless the requested format is "stdh"
+    # which contains the header from npemwinstatcounters).
     set r "";
-    foreach key [list \
-		     data_type \
-		     data_format \
-		     start_time \
-		     upstream_master \
-		     npemwin_status \
-		     num_clients \
-		     client_table] {
-	if {$format eq "csv"} {
-	    eval append r $key "=" $$key;
-	    append r ";\n";
+    foreach k [list data_type] {
+	if {$format eq "std"} {
+	    append r "$k = $iemwin($k)\n";
+	} elseif {$format eq "csv"} {
+	    append r "$iemwin($k),";
+	} elseif {$format eq "csvk"} {
+	    append r "$k=$iemwin($k),";
 	} elseif {$format eq "xml"} {
-	    eval append r "<$key>" $$key "</$key>\n";
+	    set r "<$k>$iemwin($k)</$k>\n";
 	}
     }
 
-    return [string trimright $r ";\n"];
+    append r [string trim $data];
+
+    return $r;
 }
 
 proc iemwin_output_xml_start {type} {
 
-    set r "<?iemwin_${type} version=\"1.0\"?>\n";
+    set r "<?${type} version=\"1.0\"?>\n";
 
     return $r;
 }
 
 proc iemwin_output_xml_end {type} {
 
-    set r "</iemwin_${type}>\n";
+    set r "</${type}>\n";
 
     return $r;
-}
-
-#
-# auxiliary functions
-#
-proc iemwin_output_stats_connections {file} {
-#
-# This function returns a tcl list, in which each element is the data
-# for each connected client. The code is copied from npemwin_connections{}
-# in functions.tcl in the main lib.
-# The data for each client is: "ip" "type" "queue" "time"
-#
-
-    set r [list];
-    set f [open $file r];
-    while {[gets $f finfo] > 0} {
-
-        if {[regexp {^(-|\s*$)} $finfo]} {
-            continue;
-        }
-
-        array set a [proc_stringtoarray [string trimleft $finfo]];
-        set client_data [format "%s %s %s %s" $a(1) $a(2) $a(3) $a(4)];
-	lappend r $client_data;
-
-    }
-    close $f;
-
-    return $r;
-}
-
-proc iemwin_get_upstream_master {} {
-				 
-    global iemwin;
-
-    set upstream_servers_status_list \
-	[split [exec cat $iemwin(upstream_master_file)] "\n"];
-    
-    # The current server is the one with the disconnect time equal to 0
-    foreach server $upstream_servers_status_list {
-	set disconnect_time [lindex $server 3];
-	if {$disconnect_time == 0} {
-	    break;
-	}
-    }
-
-    return $server;
 }
