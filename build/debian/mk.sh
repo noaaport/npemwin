99c8cedce4871974e5d8146f4e301a5f450fcfd2
Jose F Nieves <nieves@volkron.net>
2014-05-18 20:08:06 -0400

    Updated debian/mk.sh to handle different control files for ubuntu-14.04 and debian.

--- build/debian/mk.sh
+++ build/debian/mk.sh
@@ -6,13 +6,14 @@ cd ../../..
 . ./${branchname}/VERSION
 
 rm -rf ${name}-${version}
-cp -r $branchname ${name}-${version}
+cp -R $branchname ${name}-${version}
 
 cd ${name}-${version}
+(cd build/debian; ./mk-control.sh)
+rm -rf debian
 cp -R build/debian .
 dpkg-buildpackage -rfakeroot -uc -us
 cp ../${name}_${version}*.deb build/debian
-rm -rf debian
 
 cd build/debian
 ./ckplist.sh ${name}_${version}*.deb
